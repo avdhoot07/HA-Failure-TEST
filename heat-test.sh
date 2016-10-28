f3989f2b5b2fb7e8cb1b251c52e6974c4d55c42d
Avdhoot Patankar <avdhoot.patankar@gmail.com>
2016-10-28 11:34:40 +0000

    HA Failure Test

--- heat-test.sh
+++ heat-test.sh
@@ -1,15 +1,15 @@
 #################
-###HEAT services down on Controller 10
+###HEAT services down on Controller 1
 ###################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: heatservicedown
   tasks:
 
 
-  - name: HEAT services down on Controller 10
+  - name: HEAT services down on Controller 1
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -19,13 +19,13 @@
 ###HEAT check
 ###################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: heatcheck
   tasks:
 
-  - name: heat check on node-10
+  - name: heat check on node-1
     shell: sh -x /root/avdhoot/heat-testing/heat-1.sh
     ignore_errors: yes
     register: png
@@ -33,33 +33,33 @@
   - pause: prompt="Verify if HEAT is PASSED, press ENTER to continue"
 
 #################
-#####HEAT services up on Controller 10
+#####HEAT services up on Controller 1
 #####################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: heatserviceup
   tasks:
 
-  - name: HEAT services start on Controller 10
+  - name: HEAT services start on Controller 1
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i start; done
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
 
 #################
-####HEAT services down on Controller 11
+####HEAT services down on Controller 2
 ####################
 
-- hosts: controller-11
+- hosts: controller-2
   gather_facts: yes
   remote_user: root
   tags: heatservicedown
   tasks:
 
 
-  - name: HEAT services down on Controller 11
+  - name: HEAT services down on Controller 2
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -69,13 +69,13 @@
 ####HEAT check
 ####################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: heatcheck
   tasks:
 
-  - name: heat check on node-10
+  - name: heat check on node-1
     shell: sh -x /root/avdhoot/heat-testing/heat-2.sh
     ignore_errors: yes
     register: png
@@ -83,32 +83,32 @@
   - pause: prompt="Verify if HEAT is PASSED, press ENTER to continue"
 
 #################
-######HEAT services up on Controller 11
+######HEAT services up on Controller 2
 ######################
 
-- hosts: controller-11
+- hosts: controller-2
   gather_facts: yes
   remote_user: root
   tags: heatserviceup
   tasks:
 
-  - name: HEAT services start on Controller 11
+  - name: HEAT services start on Controller 2
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i start; done
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
 
 #################
-#######HEAT services down on Controller 13
+#######HEAT services down on Controller 3
 #######################
 
-- hosts: controller-13
+- hosts: controller-3
   gather_facts: yes
   remote_user: root
   tags: heatservicedown
   tasks:
 
-  - name: HEAT services start on Controller 13
+  - name: HEAT services start on Controller 3
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -118,13 +118,13 @@
 ######HEAT check
 ######################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: heatcheck
   tasks:
 
-  - name: sahara check on node-10
+  - name: sahara check on node-1
     shell: sh -x /root/avdhoot/heat-testing/heat-3.sh
     ignore_errors: yes
     register: png
@@ -132,16 +132,16 @@
   - pause: prompt="Verify if HEAT is PASSED, press ENTER to continue"
 
 #################
-#######HEAT services up on Controller 13
+#######HEAT services up on Controller 3
 #######################
 
-- hosts: controller-13
+- hosts: controller-3
   gather_facts: yes
   remote_user: root
   tags: heatserviceup
   tasks:
 
-  - name: HEAT services start on Controller 13
+  - name: HEAT services start on Controller 3
     shell: for i in `cd /etc/init.d/ ; ls heat-*`; do service $i start; done
     ignore_errors: yes
     register: png
--- murano-test.sh
+++ murano-test.sh
@@ -1,15 +1,15 @@
 #################
-###MURANO services down on Controller 10
+###MURANO services down on Controller 1
 ###################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: muranoservicedown
   tasks:
 
 
-  - name: MURANO services down on Controller 10
+  - name: MURANO services down on Controller 1
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -25,24 +25,24 @@
   tags: muranocheck
   tasks:
 
-  - name: murano check on node-10
-    shell: export http_proxy=http://10.135.80.164:8678; export https_proxy=http://10.135.80.164:8678; bash /root/test_murano.sh
+  - name: murano check on node-1
+    shell: bash /root/test_murano.sh
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
   - pause: prompt="Verify if tempest is PASSED, press ENTER to continue"
 
 #################
-#####MURANO services up on Controller 10
+#####MURANO services up on Controller 1
 #####################
 
-- hosts: controller-10
+- hosts: controller-1
   gather_facts: yes
   remote_user: root
   tags: muranoserviceup
   tasks:
 
-  - name: MURANO services start on Controller 10
+  - name: MURANO services start on Controller 1
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i start; done
     ignore_errors: yes
     register: png
@@ -50,17 +50,17 @@
   - pause: prompt="Verify if tempest is PASSED, press ENTER to continue"
 
 #################
-####MURANO services down on Controller 11
+####MURANO services down on Controller 2
 ####################
 
-- hosts: controller-11
+- hosts: controller-2
   gather_facts: yes
   remote_user: root
   tags: muranoservicedown
   tasks:
 
 
-  - name: MURANO services down on Controller 11
+  - name: MURANO services down on Controller 2
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -76,40 +76,40 @@
   tags: muranocheck
   tasks:
 
-  - name: murano check on node-10
-    shell: export http_proxy=http://10.135.80.164:8678; export https_proxy=http://10.135.80.164:8678; bash /root/test_murano.sh
+  - name: murano check on node-1
+    shell: bash /root/test_murano.sh
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
   - pause: prompt="Verify if tempest is PASSED, press ENTER to continue"
 
 #################
-######MURANO services up on Controller 11
+######MURANO services up on Controller 2
 ######################
 
-- hosts: controller-11
+- hosts: controller-2
   gather_facts: yes
   remote_user: root
   tags: muranoserviceup
   tasks:
 
-  - name: MURANO services start on Controller 11
+  - name: MURANO services start on Controller 2
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i start; done
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
 
 #################
-#######MURANO services down on Controller 13
+#######MURANO services down on Controller 3
 #######################
 
-- hosts: controller-13
+- hosts: controller-3
   gather_facts: yes
   remote_user: root
   tags: muranoservicedown
   tasks:
 
-  - name: MURANO services stop on Controller 13
+  - name: MURANO services stop on Controller 3
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i stop; done
     ignore_errors: yes
     register: png
@@ -125,24 +125,24 @@
   tags: muranocheck
   tasks:
 
-  - name: murano check on node-10
-    shell: export http_proxy=http://10.135.80.164:8678; export https_proxy=http://10.135.80.164:8678; bash /root/test_murano.sh
+  - name: murano check on node-1
+    shell: bash /root/test_murano.sh
     ignore_errors: yes
     register: png
   - debug: var=png.stdout
   - pause: prompt="Verify if tempest is PASSED, press ENTER to continue"
 
 #################
-#######MURANO services up on Controller 13
+#######MURANO services up on Controller 3
 #######################
 
-- hosts: controller-13
+- hosts: controller-3
   gather_facts: yes
   remote_user: root
   tags: muranoserviceup
   tasks:
 
-  - name: MURANO services start on Controller 13
+  - name: MURANO services start on Controller 3
     shell: for i in `cd /etc/init.d/ ; ls openstack-murano-*`; do service $i start; done
     ignore_errors: yes
     register: png
